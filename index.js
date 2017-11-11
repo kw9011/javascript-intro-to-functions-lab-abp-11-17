a7299009ff2efd8ba8139a8a516e5a5009c1d6ad
Karmy Widjaja <karmy0311@gmail.com>
2017-11-11 15:39:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ function shout (string) {
 function whisper (string) {
   return string. toLowerCase()
 }
-function shout (string){
-  console.log (string)
+function logShout(string) {
+  console.log(string.toUpperCase())
 }
+function logWhisper (string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  }
